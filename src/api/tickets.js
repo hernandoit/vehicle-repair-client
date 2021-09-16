62d3205954d057d924cb95535f43ec81caa96d20
hernandoit <hernandohernandezit@gmail.com>
2021-09-16 17:55:27 -0400

    add api axios routes for tickets

---
+++ src/api/tickets.js
@@ -0,0 +1,53 @@
+// apiUrl will be either the production or development url defined
+// in the apiConfig.js file
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// Create Ticket Request
+// assume that `data` is an object with `title` and `director`
+// { title: 'something', director: 'someone' }
+// POST /tickets/:carId
+export const addTicket = (data, carId, user) => {
+  return axios({
+    url: apiUrl + '/tickets/' + carId,
+    method: 'post',
+    data: { ticket: data },
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
+
+// GET /tickets/:carId/:ticketId
+export const showTickets = (carId, ticketId, user) => {
+  return axios({
+    url: apiUrl + '/tickets/' + carId + ticketId,
+    // method is optional, default is GET
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
+
+// PATCH /tickets/:carId/:ticketId
+export const updateTicket = (data, carId, ticketId, user) => {
+  return axios({
+    url: apiUrl + '/tickets/' + carId + ticketId,
+    method: 'PATCH',
+    data: { ticket: data },
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
+
+// DELETE /tickets/:carId/:ticketId
+export const destroyTicket = (carId, ticketId, user) => {
+  return axios({
+    url: apiUrl + '/tickets/' + carId + ticketId,
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
