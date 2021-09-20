bc6330ca7317af7b9aa1c3c5cee9a6898eb746f5
hernandoit <hernandohernandezit@gmail.com>
2021-09-20 15:26:22 -0400

    fix functionality to show tickets to edit on show cars

--- src/components/tickets/ShowTicket.js
+++ src/components/tickets/ShowTicket.js
@@ -18,7 +18,9 @@ class ShowTicket extends Component {
 
   componentDidMount () {
     // one of the automatic router props we get is the match object - that has data about the params in our front-end route url
+    // const { match, user, msgAlert } = this.props
     const { match, user, msgAlert } = this.props
+
     showTicket(match.params.carId, match.params.ticketId, user)
       .then(res => this.setState({ ticket: res.data.ticket }))
       .then(() => msgAlert({
@@ -48,30 +50,23 @@ class ShowTicket extends Component {
     }
 
     // Get the owner (a user id) from the car state
-    const { owner, tickets } = this.state.ticket // job, labor, isComplete,
-    const { history, match, user } = this.props
+    const { history, match } = this.props
 
     return (
       <>
         <h1>Vehicle Ticket(s)</h1>
-        {tickets.map(({ id, job, labor, isComplete }) => (
-          <ShowTicket key={id} job={job} labor={labor} isComplete={isComplete} />
-        ))}
+        <p>Iam in show ticket</p>
         {/* <h3>Vehicle Ticket(s)</h3>
         { tickets.map(ticket => <div>{ticket.key} {ticket.job} {ticket.labor} {ticket.isComplete}</div>) } */}
         {/* <h5>{job} {labor} {isComplete}</h5> */}
         {/* Compare the signed in user's ID against the owner of this car */}
-        {user._id === owner && (
-          <>
-            <Button onClick={this.handleDelete}>Delete</Button>
-            {/* Button with a Link inside should work but is ugly. Better way below. */}
-            {/* <Button><Link to={`/cars/${match.params.id}/edit`}>Update</Link></Button> */}
-            {/* Provide the Button a `onClick` handler & use the history object to redirect the user */}
-            <Button onClick={() => history.push(`/tickets/${match.params.carId}/${match.params.ticketId}/edit`)}>
-              Update
-            </Button>
-          </>
-        )}
+        <Button onClick={this.handleDelete}>Delete</Button>
+        {/* Button with a Link inside should work but is ugly. Better way below. */}
+        {/* <Button><Link to={`/cars/${match.params.id}/edit`}>Update</Link></Button> */}
+        {/* Provide the Button a `onClick` handler & use the history object to redirect the user */}
+        <Button onClick={() => history.push(`/tickets/${match.params.carId}/${match.params.ticketId}/edit`)}>
+          Update
+        </Button>
       </>
     )
   }
