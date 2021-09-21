65e0696594821e28ff202ae43e1ba25a539dcd19
hernandoit <hernandohernandezit@gmail.com>
2021-09-21 08:52:39 -0400

    add showTicket view to showCar

--- src/components/cars/ShowCars.js
+++ src/components/cars/ShowCars.js
@@ -2,22 +2,20 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 // API request
 import { showCars, destroyCar } from '../../api/cars'
-// import { addTicket } from '../../api/tickets'
+
 import Button from 'react-bootstrap/Button'
-// import ShowTicket from '../tickets/ShowTicket'
+import ShowTicket from '../tickets/ShowTicket'
 
 class ShowCars extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      // using null as a starting value will help us manage the "loading state" of our ShowCars component
       car: null
     }
   }
 
   componentDidMount () {
-    // one of the automatic router props we get is the match object - that has data about the params in our front-end route url
     const { match, user, msgAlert } = this.props
     showCars(match.params.id, user)
       .then(res => this.setState({ car: res.data.car }))
@@ -36,7 +34,6 @@ class ShowCars extends Component {
   handleDelete = (event) => {
     const { match, user, msgAlert, history } = this.props
     destroyCar(match.params.id, user)
-      // Redirect to the list of cars
       .then(() => history.push('/cars'))
       .then(() => msgAlert({ heading: 'Deleted car successfully', message: 'car is no more', variant: 'success' }))
       .catch(err => msgAlert({ heading: 'Delete car failed :(', message: 'Something went wrong: ' + err.message, variant: 'danger' }))
@@ -49,26 +46,23 @@ class ShowCars extends Component {
 
     // Get the owner (a user id) from the car state
     // owner
-    const { year, make, model, tickets } = this.state.car
+    const { year, make, model } = this.state.car
     // user
     const { history, match } = this.props
-    // console.log('this is state', this.state)
-    console.log('THIS IS TICKETS', tickets)
+    const { car } = this.state
+
+    const showTickets = car.tickets.map(ticket => (
+      <ShowTicket key={ticket.id} job={ticket.job} labor={ticket.labor} isComplete={ticket.isComplete} />
+    ))
     return (
       <>
         <div>
-          <h3>Vehicle Ticket(s)</h3>
-          <h5>{year} {make} {model}</h5>
+          <h1>{year} {make} {model}</h1>
           <Button onClick={this.handleDelete}>Delete Vehicle</Button>
           <Button onClick={() => history.push(`/cars/${match.params.id}/edit`)}>Update Vehicle</Button>
           <Button onClick={() => history.push(`/add-tickets/${match.params.id}`)}>Add Ticket</Button>
         </div>
-        <p>Vehicle Tickets</p>
-        {/* Compare the signed in user's ID against the owner of this car */}
-        {/* {user._id === owner && (
-          <>
-          </>
-        )} */}
+        <p>{showTickets}</p>
       </>
     )
   }
